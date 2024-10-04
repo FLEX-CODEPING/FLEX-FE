a7ee7730409ea26ebaa5da5e64c7b573ec617024
Minkyu0424 <yng1404@gmail.com>
2024-10-04 21:48:44 +0900

    FLEX-86 feat : 헤더 컴포넌트 제작

--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import '../styles/globals.css';
 
 export const metadata: Metadata = {
@@ -6,13 +7,23 @@ export const metadata: Metadata = {
   description: '내 곁에 든든한 재테크 친구! Flex',
 };
 
+const pretendard = localFont({
+  src: '../static/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className="min-h-screen min-w-[800px] mx-auto">
+    <html
+      lang="ko"
+      className={`${pretendard.variable} min-h-screen min-w-[800px] mx-auto font-pretendard`}
+    >
       <body>{children}</body>
     </html>
   );
--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Header from '../components/common/layout/Header';
 import { TITLE } from '../constants/main';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="relative w-full min-h-screen">
+      <Header />
       <Link href="/test">{TITLE}</Link>
     </div>
   );
