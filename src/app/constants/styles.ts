9e348eda15307484998d93444688ceaedf0bf63e
Minkyu0424 <yng1404@gmail.com>
2024-10-06 18:15:56 +0900

    FLEX-86 refactor : 불필요 속성 삭제

--- src/app/constants/styles.ts
+++ src/app/constants/styles.ts
@@ -2,8 +2,6 @@ export const BUTTON_STYLE = {
   default: (className: string) => `w-full ${className}`,
   signUp: (className: string) =>
     `w-full bg-main-1 text-white h-10 rounded-lg ${className}`,
-  complete: (className: string) =>
-    `w-[640px] h-12 text-white bg-main-1 rounded-lg ${className}`,
 } as const;
 
 export const INPUT_STYLE = {
