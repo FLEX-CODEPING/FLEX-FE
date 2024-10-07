3cb79cd3da8332a86595c9e5f5b66d7a56054e13
Minkyu0424 <yng1404@gmail.com>
2024-10-07 19:20:56 +0900

    FLEX-91 refactor : 헤더 크기 수정, 로고 링크적용

--- src/app/components/common/layout/Header.tsx
+++ src/app/components/common/layout/Header.tsx
@@ -10,11 +10,11 @@ export const dela = Dela_Gothic_One({
 
 function Header() {
   return (
-    <header className="w-full flex items-center justify-between h-[108px] px-[5%] text-2xl">
-      <div className="flex gap-x-2 items-center">
+    <header className="w-full flex items-center justify-between h-[108px] px-[5%] text-xl">
+      <Link className="flex gap-x-2 items-center" href="/">
         <Image width={40} height={40} src="/Images/logo.png" alt="logo" />
         <p className={`${dela.className} text-[28px] text-main-1`}>FLEX</p>
-      </div>
+      </Link>
       <div className="flex">
         {HEADER_TEXT.map((text, i) => (
           <p className="mr-[50px]" key={text}>
@@ -24,13 +24,13 @@ function Header() {
       </div>
       <div className="flex items-center gap-x-4">
         <Link
-          className="flex-center w-[72px] h-[36px] px-1.5 py-1 bg-main-1 text-white text-base font-semibold rounded-[15px]"
+          className="flex-center w-[64px] h-[28px] px-1.5 py-1 bg-main-1 text-white text-xs font-semibold rounded-[15px]"
           href="/auth/signIn"
         >
           {LOGIN_TEXT[0]}
         </Link>
-        <div className="text-main-1 text-[28px] font-light">|</div>
-        <Link className="text-base font-bold" href="/auth/signUp">
+        <div className="text-main-1 text-[24px] font-light">|</div>
+        <Link className="text-xs font-bold" href="/auth/signUp">
           {LOGIN_TEXT[1]}
         </Link>
       </div>
