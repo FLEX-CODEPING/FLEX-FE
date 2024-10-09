504ef900f663a51323bc1167103dccdac488aa8f
김낙도 <tpcmsk0920@gmail.com>
2024-10-10 00:06:26 +0900

    [feat] : 마크다운 에디터 컬러피커 적용

--- src/app/components/markdown/MyEditor.tsx
+++ src/app/components/markdown/MyEditor.tsx
@@ -1,21 +1,32 @@
-'use client'
+'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Editor } from '@toast-ui/react-editor';
+import "tui-color-picker/dist/tui-color-picker.css";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 import '@toast-ui/editor/dist/toastui-editor.css';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+
 
 const MyEditor = () => {
 
+
   return (
     <Editor
-    height="600px" 
-    initialEditType="markdown"
-    initialValue="## 본문 내용을 작성해 주세요." 
-    hideModeSwitch={true}
+      height="600px"
+      initialEditType="markdown"
+      initialValue="### 본문 내용을 작성해 주세요."
+      hideModeSwitch={true}
+      previewStyle="vertical"
+      plugins={[colorSyntax]}
       toolbarItems={[
-        ['heading', 'bold'],
+        ['heading', 'bold','italic', 'strike'],
         ['ul', 'ol'],
         ['code', 'codeblock'],
+        ['table'],
+        ['link'],
+        ['image'],
+        ['scrollSync'],
       ]}
     />
   );
