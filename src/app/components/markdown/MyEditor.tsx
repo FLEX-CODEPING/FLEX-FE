63d1a436781b749b36126e7a78dcef51d5c7ffa7
김낙도 <tpcmsk0920@gmail.com>
2024-10-10 10:29:30 +0900

    [test] : 에디터 작성 후 HTML 출력 데이터 조회 테스트

--- src/app/components/markdown/MyEditor.tsx
+++ src/app/components/markdown/MyEditor.tsx
@@ -1,37 +1,59 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
-import { Editor } from '@toast-ui/react-editor';
+import React, { FormEventHandler, useEffect, useRef, useState } from 'react';
+import { Editor, Viewer } from '@toast-ui/react-editor';
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 
 const MyEditor = () => {
+  const editorRef = useRef<Editor>(null);
+  const [getContent, setGetContent] = useState('');
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    const content = editorRef.current?.getInstance().getHTML();
+
+    setGetContent(content);
+    console.log('출력 데이터', content);
+  };
+
   return (
     <div>
-      <Editor
-        height="600px"
-        initialEditType="markdown"
-        initialValue="### 본문 내용을 작성해 주세요."
-        hideModeSwitch={true}
-        previewStyle="vertical"
-        plugins={[colorSyntax]}
-        toolbarItems={[
-          ['heading', 'bold', 'italic', 'strike'],
-          ['ul', 'ol'],
-          ['code', 'codeblock'],
-          ['table'],
-          ['link'],
-          ['image'],
-          ['scrollSync'],
-        ]}
-      />
-      <div className='mt-10 flex justify-end'>
-        <button className="w-36 h-10 bg-[#cbcaca] text-white rounded-[10px] font-bold text-xl ">
-          출간하기
-        </button>
+      <form onSubmit={onSubmit}>
+        <Editor
+          ref={editorRef}
+          height="600px"
+          initialEditType="markdown"
+          initialValue="### 본문 내용을 작성해 주세요."
+          hideModeSwitch={true}
+          previewStyle="vertical"
+          plugins={[colorSyntax]}
+          toolbarItems={[
+            ['heading', 'bold', 'italic', 'strike'],
+            ['ul', 'ol'],
+            ['code', 'codeblock'],
+            ['table'],
+            ['link'],
+            ['image'],
+            ['scrollSync'],
+          ]}
+        />
+        <div className="mt-10 flex justify-end">
+          <button
+            type="submit"
+            className="w-36 h-10 bg-[#cbcaca] text-white rounded-[10px] font-bold text-xl "
+          >
+            출간하기
+          </button>
+        </div>
+      </form>
+      <div className="w-[983px] flex gap-8 mt-[40px] justify-center mb-[10px]">
+        <div className="text-xl font-bold">출력된 HTML</div>
       </div>
+      <div className="w-[983px] flex gap-8 justify-center">{getContent}</div>
     </div>
   );
 };
