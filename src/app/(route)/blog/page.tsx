d43b9ce2d24d8c7a7dbb18ce3cd08e4dcbc31131
kangai1 <kmj104506@gmail.com>
2024-10-12 02:21:39 +0900

    FLEX-90 feat: 블로그 페이지 렌더링

---
+++ src/app/(route)/blog/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import BlogContainer from '@/app/components/blog/BlogContainer';  
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1>블로그 페이지</h1>
+      <BlogContainer />  
+    </div>
+  );
+}
