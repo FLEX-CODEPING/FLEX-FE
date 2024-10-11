56e083ea1b4f6b086eb29ed4007a34f79645cd44
kangai1 <kmj104506@gmail.com>
2024-10-12 02:22:48 +0900

    FLEX-90 feat: 블로그 검색 페이지 렌더링

---
+++ src/app/(route)/blog/paseS.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import BlogSearch from '@/app/components/blog/BlogSearch';  
+
+export default function BlogSearchPage() {
+  return (
+    <div>
+      <h1>블로그 검색</h1>
+      <BlogSearch /> 
+    </div>
+  );
+}
