fcd881eb6382bef673b10581f1fc8902a9389299
kangai1 <kmj104506@gmail.com>
2024-10-13 00:44:50 +0900

    FLEX-90 feat: 블로그 컨텐츠 분류

---
+++ src/app/constants/BlogConstants.ts
@@ -0,0 +1,12 @@
+export const AGE_OPTIONS = ['~20', '21-30', '31-40', '41-50', '50+'];
+
+export const SALARY_OPTIONS = [
+  '소득 없음',
+  '3천 이하',
+  '5천 이하',
+  '7천 이하',
+  '1억 이하',
+  '2억 이상',
+];
+
+export const NAV_OPTIONS = ['전체', '추천', '팔로잉'];
