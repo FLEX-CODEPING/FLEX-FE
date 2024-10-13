1f4ec3a0128ba64d69a51248e86e8d87ee4d6c48
kangai1 <kmj104506@gmail.com>
2024-10-13 20:01:53 +0900

    FLEX-90 feat: 블로그 검색 페이지

---
+++ src/app/components/blog/Pagination.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  handlePageChange: (pageNumber: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, handlePageChange }) => {
+  return (
+    <div className="pagination">
+      {[...Array(totalPages)].map((_, index) => (
+        <button
+          key={index + 1}
+          onClick={() => handlePageChange(index + 1)}
+          className={`page-button ${currentPage === index + 1 ? 'active-page' : ''}`}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;
