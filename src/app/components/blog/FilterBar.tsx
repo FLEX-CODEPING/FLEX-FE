7ae1e005565c160e9cf883f805c99fea7c5c7cc6
kangai1 <kmj104506@gmail.com>
2024-10-14 12:44:27 +0900

    FLEX-90 fix : 변경 없음

--- src/app/components/blog/FilterBar.tsx
+++ src/app/components/blog/FilterBar.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import Image from 'next/image';
 
 interface FilterBarProps {
-  onFilterSelect: (selectedFilters: string[]) => void; // 필터 선택 시 호출할 함수
+  onFilterSelect: (selectedFilters: string[]) => void; 
 }
 
 export const LabelNormal = (): JSX.Element => {
   return (
     <div className="inline-flex h-10 items-center justify-center gap-[15px] px-5 py-0.5 relative rounded-[20px] overflow-hidden border border-solid border-main-1">
       <div className="inline-flex items-center justify-center gap-[3px] relative flex-[0_0_auto]">
-        {/* 이미지나 아이콘을 적절히 표시 */}
+       
         <Image
             src="/images/star.png"
             alt="좋아요"
@@ -21,7 +20,7 @@ export const LabelNormal = (): JSX.Element => {
         </div>
       </div>
 
-      {/* 필터 1: 3천 이하 */}
+      
       <div className="relative w-[66px] h-7">
         <div className="relative w-16 h-7 bg-[url(/rectangle-28.svg)] bg-cover">
           <div className="top-[3px] left-2 absolute font-medium text-black text-xs tracking-[1.08px] leading-[20.5px] whitespace-nowrap">
@@ -30,7 +29,7 @@ export const LabelNormal = (): JSX.Element => {
         </div>
       </div>
 
-      {/* 필터 2: 21 ~ 30 */}
+     
       <div className="relative w-[66px] h-7">
         <div className="relative w-16 h-7 bg-[url(/image.svg)] bg-cover">
           <div className="top-1 left-[7px] absolute font-medium text-black text-xs tracking-[1.08px] leading-[20.5px] whitespace-nowrap">
@@ -39,7 +38,7 @@ export const LabelNormal = (): JSX.Element => {
         </div>
       </div>
 
-      {/* 필터 3: 해외주식 */}
+      
       <div className="relative w-[66px] h-7">
         <div className="relative w-16 h-7 bg-[url(/rectangle-28-2.svg)] bg-cover">
           <div className="top-1 left-2 absolute font-medium text-black text-xs tracking-[1.08px] leading-[20.5px] whitespace-nowrap">
@@ -48,7 +47,7 @@ export const LabelNormal = (): JSX.Element => {
         </div>
       </div>
 
-      {/* 필터 4: 부동산 */}
+      
       <div className="relative w-[66px] h-7">
         <div className="relative w-16 h-7 bg-[url(/rectangle-28-3.svg)] bg-cover">
           <div className="top-1 left-3.5 absolute font-medium text-black text-xs tracking-[1.08px] leading-[20.5px] whitespace-nowrap">
