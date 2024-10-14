5f23d8770ee2bc97edcd0c467bd05810344f7fda
kangai1 <kmj104506@gmail.com>
2024-10-14 12:46:55 +0900

    FLEX-90 fix : 주석 제거

--- src/app/components/blog/Result.tsx
+++ src/app/components/blog/Result.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BlogPost } from '../../_types/blog'; // 정확한 경로 확인
+import { BlogPost } from '../../_types/blog'; 
 import PostCard from './PostCard';
 
 interface ResultsProps {
@@ -23,26 +23,26 @@ const Results: React.FC<ResultsProps> = ({
 
   return (
     <>
-      {/* 검색이 실행되면 결과 개수 텍스트를 표시 */}
+      
       {searchExecuted && results.length > 0 && (
         <div className="result-count">
           <p>총 {results.length}개의 포스트를 찾았습니다.</p>
         </div>
       )}
 
-      {/* 로딩 상태 */}
+     
       {loading ? (
         <div className="loading-container">
           <p>로딩 중...</p>
         </div>
       ) : searchExecuted && currentPosts.length === 0 ? (
-        /* 검색 결과가 없을 때 */
+        
         <div className="no-results-container">
           <img src="/images/2c.png" alt="no-results" className="no-results-image" />
           <p>검색 결과가 없습니다.</p>
         </div>
       ) : (
-        /* 검색 결과가 있을 때 */
+        
         <div className="posts-container">
           {currentPosts.map((post) => (
             <PostCard key={post.id} post={post} />
