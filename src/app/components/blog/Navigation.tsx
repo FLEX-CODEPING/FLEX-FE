7e497519de3269cef36332dd3f5c3b28a68c5aa4
kangai1 <kmj104506@gmail.com>
2024-10-14 12:46:14 +0900

    FLEX-90 fix : 주석 제거

--- src/app/components/blog/Navigation.tsx
+++ src/app/components/blog/Navigation.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import Link from 'next/link';
 import { NAV_OPTIONS } from '@/app/constants/BlogConstants';
-import { LabelNormal } from './FilterBar'; // LabelNormal 컴포넌트 불러오기
+import { LabelNormal } from './FilterBar'; 
+import Image from 'next/image';
 
 interface NavigationProps {
   selectedNav: string;
@@ -9,39 +10,47 @@ interface NavigationProps {
 }
 
 const Navigation: React.FC<NavigationProps> = ({ selectedNav, handleNavClick }) => {
-  const [showLabelNormal, setShowLabelNormal] = useState(false); // 추천을 눌렀을 때 LabelNormal 표시 여부 상태
+  const [showLabelNormal, setShowLabelNormal] = useState(false); 
 
-  // 내비게이션 버튼 클릭 핸들러
+  
   const handleNavSelect = (nav: string) => {
     handleNavClick(nav);
     if (nav === '추천') {
-      setShowLabelNormal(true); // 추천을 선택했을 때 LabelNormal을 보여줌
+      setShowLabelNormal(true); 
     } else {
-      setShowLabelNormal(false); // 다른 네비게이션 항목을 선택하면 숨김
+      setShowLabelNormal(false); 
     }
   };
 
   return (
     <div className="w-full">
-      <div className="nav-section flex justify-between items-center p-4 border-b border-gray-300">
+      <div className="nav-section flex justify-between items-center p-4">
         <div className="nav-options flex items-center gap-6">
           {NAV_OPTIONS.map((nav) => (
             <button
               key={nav}
               className={`nav-button ${selectedNav === nav ? 'selected' : ''}`}
-              onClick={() => handleNavSelect(nav)} // 클릭 시 handleNavSelect 함수 호출
+              onClick={() => handleNavSelect(nav)} 
+              
             >
               {nav}
             </button>
           ))}
           
           <Link href="/blog/blogsearch" className="nav-button">
-            검색
+      <div className="icon-container">
+      <Image
+          src="/images/2c.png"
+          alt="2cImg"
+          width={18}
+          height={18}
+        />
+      </div>
           </Link>
         </div>
       </div>
 
-      {/* 추천을 선택했을 때 LabelNormal 표시 */}
+      
       {showLabelNormal && <LabelNormal />}
     </div>
   );
