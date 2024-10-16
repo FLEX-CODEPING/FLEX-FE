e0f50758857018b86a3fc0a0edd384474bbaae93
kangai1 <kmj104506@gmail.com>
2024-10-16 11:49:39 +0900

    FLEX-90 fix : 폴더 경로 변경

--- src/app/(route)/blog/blogsearch/page.tsx
+++ src/app/(route)/blog/blogsearch/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import BlogSearch from '@/app/components/blog/BlogSearch';  
+import BlogSearch from '@/app/components/blogmain/BlogSearch';  
 
 export default function BlogSearchPage() {
   return (
     <div>
-      <h1>블로그 검색</h1>
+      <h1></h1>
       <BlogSearch /> 
     </div>
   );
