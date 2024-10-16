d1847ce67e59bcfbae40cf5a21cc5742e47c41d3
kangai1 <kmj104506@gmail.com>
2024-10-16 11:49:14 +0900

    FLEX-90 feat : 전체 경로 추가

---
+++ src/app/(route)/blog/all/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import BlogContainer from '@/app/components/blogmain/BlogContainer'; 
+
+const BlogPage = () => {
+  return (
+    <div>
+      <BlogContainer />
+    </div>
+  );
+};
+
+export default BlogPage;
