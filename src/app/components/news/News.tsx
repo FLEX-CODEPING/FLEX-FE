71b6d69efa1efa239e515bb612481fb3498decd2
kangai1 <kmj104506@gmail.com>
2024-10-18 22:35:25 +0900

    FLEX-123 fix : 뉴스 요약 필터바 위치 수정

--- src/app/components/news/News.tsx
+++ src/app/components/news/News.tsx
@@ -1,5 +1,6 @@
-"use client";
-
+'use client';
+import Icons from '@/app/components/common/Icons';
+import { likeSmall, title, k } from '@/app/constants/iconPath';
 import React, { useState } from 'react';
 import FilterBar from '@/app/components/news/FilterBar';
 import { newsSummaries, additionalNews } from '@/app/constants/newsdata';
@@ -22,7 +23,7 @@ const News = (): JSX.Element => {
 
   return (
     <div className="news-page-container bg-white w-full max-w-[1400px] mx-auto py-12">
-      {/* 뉴스 제목 */}
+      
       <div className="news-title text-center mt-12">
         <h2 className=" text-[36px] font-normal text-black text-center leading-[60px] mb-12">
           키워드를 선택하여 <br />
@@ -30,16 +31,19 @@ const News = (): JSX.Element => {
         </h2>
       </div>
 
-      {/* 필터 바 컴포넌트 */}
+      
       <FilterBar
         selectedFilters={selectedFilters}
         handleFilterClick={handleFilterClick}
         filterOptions={filterOptions}
       />
 
-      {/* 뉴스 요약 */}
-      <div className="max-w-[1200px] mx-auto mt-12">
-        <h3 className="text-xl font-bold mb-4">{newsSummaries[0].title}</h3>
+      
+      <div className="max-w-[1200px] mx-auto mt-24">
+        <div className="flex items-center mb-4">
+          <Icons name={title} /> 
+          <h3 className="text-xl font-bold ml-2">{newsSummaries[0].title}</h3>
+        </div>
         <div className="news-items p-4 border border-[#7a7a7a] rounded-[15px]">
           <ul className="list-none space-y-2">
             {newsSummaries[0].content.map((item, index) => (
@@ -52,14 +56,15 @@ const News = (): JSX.Element => {
         </div>
       </div>
 
-      {/* 추가 뉴스 */}
+      
       <div className="mt-12 max-w-[1200px] mx-auto">
         <ul className="space-y-4">
           {additionalNews.map((news, index) => (
-            <li key={index} className="flex justify-between items-center pb-2">
-              <span className="text-sm text-gray-500">{news.date}</span>
-              <span className="font-semibold text-gray-700">{news.headline}</span>
-              <span className="text-sm text-gray-500">{news.summary}</span>
+            <li key={index} className="flex justify-between items-start pb-2">
+               <span className="text-sm text-gray-500 text-left" style={{ width: "150px" }}>{news.date}</span>
+        <span className="font-semibold text-gray-700 text-left" style={{ width: "400px" }}>{news.headline}</span>
+        <span className="text-sm text-gray-500 text-left" style={{ width: "500px" }}>{news.summary}</span>
+        <Icons name={k} />
             </li>
           ))}
         </ul>
