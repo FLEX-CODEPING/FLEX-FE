1c7d9544a416e3f17e6a4980f1c2212b4e510098
김낙도 <tpcmsk0920@gmail.com>
2024-10-23 01:57:30 +0900

    FLEX-148 feat : 게시물 작성 - 뒤로가기 버튼 생성

--- src/app/components/blog/blogmain/post/EditBlogContainer.tsx
+++ src/app/components/blog/blogmain/post/EditBlogContainer.tsx
@@ -3,36 +3,45 @@
 import dynamic from 'next/dynamic';
 import BlogInfoContainer from './BlogInfoContainer';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const MyEditor = dynamic(() => import('./markdown/MyEditor'), { ssr: false });
 
 const EditBlogContainer = () => {
   const [postData, setPostData] = useState<PostTypes>();
+  const router = useRouter();
 
   const setTitle = (title: string) => {
-    setPostData((prevPost) => ({
-      ...prevPost,
-      title,
-    } as PostTypes));
+    setPostData(
+      (prevPost) =>
+        ({
+          ...prevPost,
+          title,
+        }) as PostTypes,
+    );
   };
 
   const setTags = (tags: string[]) => {
-    setPostData((prevPost) => ({
-      ...prevPost,
-      tags,
-    } as PostTypes));
+    setPostData(
+      (prevPost) =>
+        ({
+          ...prevPost,
+          tags,
+        }) as PostTypes,
+    );
   };
 
   const setContent = (content: string) => {
-    setPostData((prevPost) => ({
-      ...prevPost,
-      content,
-    } as PostTypes));
+    setPostData(
+      (prevPost) =>
+        ({
+          ...prevPost,
+          content,
+        }) as PostTypes,
+    );
   };
 
   const handlePostSubmit = async () => {
-    
-
     try {
       const response = await fetch('/api/posts', {
         method: 'POST',
@@ -47,17 +56,29 @@ const EditBlogContainer = () => {
       }
 
       console.log('Post submitted successfully!');
+      router.push('/blog');
     } catch (error) {
       console.error('Error submitting post:', error);
     }
   };
-  
+
+  const goBack = () => {
+    router.push('/');
+  };
+
   return (
     <div className="mb-[50px]">
       <BlogInfoContainer setTitle={setTitle} setTags={setTags} />
       <div className="mt-[20px] mb-10">
         <MyEditor setContent={setContent} />
-        <div className="mt-10 flex justify-end">
+        <div className="mt-10 flex justify-between">
+          <button
+            type="submit"
+            onClick={goBack}
+            className="w-36 h-10 bg-[#000000] text-white rounded-[10px] font-bold text-xl "
+          >
+            뒤로가기
+          </button>
           <button
             type="submit"
             onClick={handlePostSubmit}
