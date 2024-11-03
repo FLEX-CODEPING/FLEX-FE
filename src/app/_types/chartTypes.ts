bec4da1d5d3741042825c38c3b35395c0fb220ea
kangai1 <kmj104506@gmail.com>
2024-11-03 22:13:02 +0900

    FLEX-171 feature : 차트 타입

---
+++ src/app/_types/chartTypes.ts
@@ -0,0 +1,18 @@
+export interface CandleData {
+    open: number;
+    low: number;
+    high: number;
+    close: number;
+    volume: number;
+    timestamp: number;
+  }
+  
+  export interface MinuteData {
+    open: number;
+    close: number;
+    high: number;
+    low: number;
+    volume: number;
+    timestamp: number;
+  }
+  
