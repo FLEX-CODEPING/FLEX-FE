f82a1d1e74a6046babf3cd1596aebf4c0dddf02a
kangai1 <kmj104506@gmail.com>
2024-11-16 18:00:55 +0900

    FLEX-171 fix : discord 타입 추가

--- src/app/constants/styles.ts
+++ src/app/constants/styles.ts
@@ -16,6 +16,8 @@ export const BUTTON_STYLE = {
     `w-[180px] h-[40px] bg-[#000000] text-white rounded-[10px] font-bold text-sm ${className}`,
   prediction: (className: string) =>
     `w-60 h-8 bg-black-1 text-white rounded-lg font-semibold text-[15px] ${className}`,
+  notification: (className: string) =>
+    `px-4 py-2 border border-gray-2 rounded-lg text-sm font-semibold ${className}`,
 } as const;
 
 export const INPUT_STYLE = {
@@ -30,4 +32,6 @@ export const INPUT_STYLE = {
     `w-[150px] h-[24px] px-3 rounded-md border-none font-light text-black-0 text-base outline-none ${className}`,
   blogName: (className: string) =>
     `w-[80%] pl-4 h-10 rounded-[10px] border border-gray-2 text-sm outline-none focus:border-main-1 ${className}`,
+  discord: (className: string) =>
+    `w-full h-10 pl-4 rounded-lg border border-blue-600 text-sm outline-none focus:border-blue-700 ${className}`,
 } as const;
