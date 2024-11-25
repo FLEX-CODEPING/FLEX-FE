44095cde074a1767c5445769fcf478296f839e5e
kangai1 <kmj104506@gmail.com>
2024-11-25 09:50:57 +0900

    FLEX-171 refactor : 중복 제거

--- src/app/_types/chartTypes.ts
+++ src/app/_types/chartTypes.ts
@@ -6,12 +6,3 @@ export interface CandleData {
   volume: number;
   timestamp: number;
 }
-
-export interface MinuteData {
-  open: number;
-  close: number;
-  high: number;
-  low: number;
-  volume: number;
-  timestamp: number;
-}
