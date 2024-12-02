f7a3803ec6b16557a3e643782341ef00fdf454ca
kangai1 <kmj104506@gmail.com>
2024-12-02 23:56:17 +0900

    FLEX-290 refactor : 주석 제거

--- src/app/components/prediction/chart/PrChartContainer.tsx
+++ src/app/components/prediction/chart/PrChartContainer.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import React from 'react';
-import PrChart from './PrChart'; // 수정된 PrChart 컴포넌트
+import PrChart from './PrChart'; 
 
 const PrChartContainer = () => {
   return (
     <div className="flex w-full px-10 py-5 rounded-[10px] border border-gray-4 flex-col justify-start items-start">
       <div className="w-full">
-        {/* PrChart 컴포넌트만 렌더링 */}
         <PrChart />
       </div>
     </div>
