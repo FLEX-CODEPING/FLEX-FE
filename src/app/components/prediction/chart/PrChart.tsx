c0a7481ce56a2627fa785d42099bc3933cade726
kangai1 <kmj104506@gmail.com>
2024-12-09 02:21:08 +0900

    FLEX-338 refactor : 차트 경로 수정

--- src/app/components/prediction/chart/PrChart.tsx
+++ src/app/components/prediction/chart/PrChart.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import React, { useEffect } from 'react';
-import UDFCompatibleDatafeed from './UDFCompatibleDatafeed';
+import PrChartContainer from './PrChartContainer';
 
-const PrChart: React.FC = () => {
+interface MockChartProps {
+  chartData: any[];
+  symbol: string;
+}
+
+const PrChart: React.FC<MockChartProps> = ({ chartData, symbol }) => {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = '/charting_library/charting_library.standalone.js';
@@ -12,20 +17,27 @@ const PrChart: React.FC = () => {
     script.onload = () => {
       if (typeof window !== 'undefined' && (window as any).TradingView) {
         const Widget = (window as any).TradingView.widget;
+
         const widgetOptions = {
+          symbol,
+          interval: '1D',
           container: 'chartContainer',
           library_path: '/charting_library/',
-          datafeed: new UDFCompatibleDatafeed(),
-          symbol: 'KRW-BTC',
-          interval: '1',
+          datafeed: new PrChartContainer(chartData), // 데이터 피드 연결
           timezone: 'Asia/Seoul',
           theme: 'Light',
           autosize: true,
-          debug: true,
+          toolbar_bg: '#f4f7f9',
+          disabled_features: [
+            'header_symbol_search', // 심볼 검색 버튼 제거
+            'symbol_search_hot_key', // 검색 단축키 비활성화
+            'symbol_info', // 심볼 정보 툴팁 제거
+            'header_compare',
+          ],
+          enabled_features: [],
         };
 
-        const tradingViewWidget = new Widget(widgetOptions);
-        console.log('TradingView widget created:', tradingViewWidget);
+        new Widget(widgetOptions);
       }
     };
 
@@ -34,9 +46,9 @@ const PrChart: React.FC = () => {
     return () => {
       document.body.removeChild(script);
     };
-  }, []);
+  }, [chartData, symbol]); // 심볼 및 데이터 변경 시 차트 재렌더링
 
-  return <div id="chartContainer" style={{ width: '100%', height: '570px' }} />;
+  return <div id="chartContainer" style={{ width: '98%', height: '570px' }} />;
 };
 
 export default PrChart;
