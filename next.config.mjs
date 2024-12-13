ae091c093807ca4273be24033f788bccf1178768
Minkyu0424 <yng1404@gmail.com>
2024-12-14 04:04:18 +0900

    285 refactor : 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,13 @@ const nextConfig = {
       '308b-203-249-127-39.ngrok-free.app',
       '4870-203-249-127-39.ngrok-free.app',
     ],
-
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '308b-203-249-127-39.ngrok-free.app',
+        pathname: '/stock-symbol/**',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
