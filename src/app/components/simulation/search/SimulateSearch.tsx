6b6b836017c5a96a768afdb38c429a0abc5fecba
Minkyu0424 <yng1404@gmail.com>
2024-12-14 12:56:36 +0900

    285 refactor : 파일 대소문자 변경

---
+++ src/app/components/simulation/search/SimulateSearch.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import useStockCodeStore from '@/app/store/store';
+import { callGet } from '@/app/utils/callApi';
+import { isOpenTime } from '@/app/utils/date';
+import { useEffect, useState } from 'react';
+import PreopenSearchInfo from './PreopenSearchInfo';
+import SearchInfo from './SearchInfo';
+import StockSearchBar from './StockSearchBar';
+
+const SimulateSearch = () => {
+  const [stockInfo, setStockInfo] = useState<null | StockInfoTypes>(null);
+  const { setStockCode, stockCode } = useStockCodeStore();
+
+  const apiURL = (code: string) => {
+    return isOpenTime()
+      ? `api/stocks?code=${code}`
+      : `api/stocks/offHour?code=${code}&date=${'2024-12-04'}`;
+  };
+
+  const getStockInfo = async (code: string) => {
+    const response = await callGet(apiURL(code));
+    if (response.isSuccess) {
+      const statusRes: InterestedStautsTypes = await callGet(
+        `api/stocks/interest/status?code=${code}`,
+      );
+      setStockCode(code, response.result.stockName);
+      setStockInfo({ ...response.result, isInterested: statusRes.result });
+    } else {
+      setStockCode('null', '');
+    }
+  };
+
+  useEffect(() => {
+    stockCode && getStockInfo(stockCode);
+  }, [stockCode]);
+
+  return (
+    <div className="flex w-full justify-between items-end pb-2">
+      {isOpenTime() ? (
+        <SearchInfo
+          stockInfo={stockInfo}
+          stockCode={stockCode}
+          getStockInfo={getStockInfo}
+        />
+      ) : (
+        <PreopenSearchInfo
+          stockInfo={stockInfo}
+          stockCode={stockCode}
+          getStockInfo={getStockInfo}
+        />
+      )}
+      <StockSearchBar getStockInfo={getStockInfo} />
+    </div>
+  );
+};
+
+export default SimulateSearch;
