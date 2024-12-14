8029459567f15e5bfe14f03b487b524c9011b778
kangai1 <kmj104506@gmail.com>
2024-12-14 19:41:40 +0900

    FLEX-338 refactor : 차트 수정

--- src/app/components/simulation/chart/ChartContainer.tsx
+++ src/app/components/simulation/chart/ChartContainer.tsx
@@ -77,28 +77,10 @@ const ChartContainer = () => {
 
   return (
     <div className="flex w-full px-5 py-5 rounded-[10px] border border-gray-4 flex-col justify-start items-start gap-y-5">
-      {/* <div className="flex w-full justify-between">
-        <div className="px-3 h-9 p-1 flex-center font-medium text-lg">
-          {CHART_TITLE[0]}
-          {selectedValue}
-        </div>
-        <div className="flex gap-x-3">
-          {CHART_VIEWTYPE.map((type) => (
-            <div
-              key={type}
-              className={`w-[25px] h-[25px] text-sm font-medium rounded-lg border border-gray-4 flex-center cursor-pointer ${type === viewType && 'bg-main-1 text-white border-none'}`}
-              onClick={() => setViewType(type)}
-            >
-              {type}
-            </div>
-          ))}
-        </div>
-      </div> */}
       {!stockCode || stockCode === 'null' ? (
         <ChartEmpty />
       ) : (
         <MockChart chartData={data} symbol={stockCode || '005930'} />
-        // <StockChart chartData={data} value={''} setValue={setSelectedValue} />
       )}
     </div>
   );
