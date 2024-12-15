01fc4af904d7d2baace153a5a2b7bdb800a3bdbc
Minkyu0424 <yng1404@gmail.com>
2024-12-15 17:28:09 +0900

    360 refactor : 인기게시물 조회수 항목, 생성날짜 추가

--- src/app/components/main/right/PopularPost.tsx
+++ src/app/components/main/right/PopularPost.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { likeSmall } from '@/app/constants/iconPath';
+import { likeSmall, viewsIcon } from '@/app/constants/iconPath';
 import { MAIN_CONTENTS_TITLE } from '@/app/constants/main';
 import { callGet } from '@/app/utils/callApi';
-import { getTodayDateBar } from '@/app/utils/date';
+import { formatDate, getTodayDateBar } from '@/app/utils/date';
 import { truncateString } from '@/app/utils/truncate';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
@@ -33,23 +33,38 @@ const PopularPost = () => {
           <NoneContent />
         </div>
       ) : (
-        <div className="w-full flex flex-col gap-y-3">
+        <div className="w-full flex flex-col gap-y-4">
           {postDatas.map((data, i) => (
             <Link
               href={`/blog/detail?id=${data.id}`}
-              className="w-full flex-col flex py-3 px-2 gap-y-1 cursor-pointer border-b border-b-gray-3 hover:border-b-main-1 transition duration-500"
+              className="w-full flex-col flex py-2 px-4 gap-y-1 cursor-pointer border-b border-b-gray-3 hover:border-b-main-1 transition duration-500"
               key={data.id}
             >
-              <div className="w-full flex justify-between">
-                <p className="text-[12px] text-orange-500 font-semibold">
-                  조회수 {data.viewCount}회
-                </p>
-                <div className="flex w-[48px] justify-between items-center">
-                  <Icons name={likeSmall} />
-                  <p className="text-sm font-medium">{data.likeCount}</p>
+              <div className="w-full h-[50px] flex justify-between">
+                <div className="h-full flex justify-between flex-col">
+                  <p className="font-semibold pt-0.5">
+                    {truncateString(data.title, 30)}
+                  </p>
+                  <p className="text-[10px] text-gray-1 font-light">
+                    {formatDate(data.createdAt)}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-y-2.5">
+                  <div className="flex justify-between items-center">
+                    <Icons
+                      name={{
+                        ...likeSmall,
+                        fill: '#F95700',
+                      }}
+                    />
+                    <p className="text-sm font-medium pl-2">{data.likeCount}</p>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <Icons name={viewsIcon} />
+                    <p className="text-sm font-medium pl-2">{data.viewCount}</p>
+                  </div>
                 </div>
               </div>
-              <p className="font-semibold">{truncateString(data.title, 120)}</p>
             </Link>
           ))}
         </div>
