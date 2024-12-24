8a9cd9b47a22248b850f5c2e9d22bb12de00abde
Minkyu0424 <yng1404@gmail.com>
2024-12-24 16:08:00 +0900

    384 refactor : 분석 api mock데이터로 대체

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -1,8 +1,8 @@
-import { getUsers } from '@/app/service/getRequest';
-import { NextResponse } from 'next/server';
+  import { getUsers } from '@/app/service/getRequest';
+  import { NextResponse } from 'next/server';
 
-export async function GET(req: Request) {
-  const data = await getUsers(req);
+  export async function GET(req: Request) {
+    const data = await getUsers(req);
 
-  return NextResponse.json(data);
-}
+    return NextResponse.json(data);
+  }
